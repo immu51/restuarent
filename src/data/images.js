2d3ebacacca6c8a4fc2f5615487093eeaeaf9c53
immu51 <khanimran57175@gmail.com>
2026-02-16 05:13:34 +0530

    svg change

--- src/data/images.js
+++ src/data/images.js
@@ -4,28 +4,28 @@
  * USED BY: Hero (HERO_IMAGES), dishes.js (DISH_IMAGES), reviews.js (AVATAR_IMAGES)
  */
 export const HERO_IMAGES = [
-  '/images/hero/1.jpg',
-  '/images/hero/2.jpg',
-  '/images/hero/3.jpg',
-  '/images/hero/4.jpg',
-  '/images/hero/5.jpg',
+  '/images/hero/1.svg',
+  '/images/hero/2.svg',
+  '/images/hero/3.svg',
+  '/images/hero/4.svg',
+  '/images/hero/5.svg',
 ]
 
 export const DISH_IMAGES = {
-  1: '/images/dishes/1.jpg',
-  2: '/images/dishes/2.jpg',
-  3: '/images/dishes/3.jpg',
-  4: '/images/dishes/4.jpg',
-  5: '/images/dishes/5.jpg',
-  6: '/images/dishes/6.jpg',
-  7: '/images/dishes/7.jpg',
-  8: '/images/dishes/8.jpg',
-  9: '/images/dishes/9.jpg',
-  10: '/images/dishes/10.jpg',
+  1: '/images/dishes/1.svg',
+  2: '/images/dishes/2.svg',
+  3: '/images/dishes/3.svg',
+  4: '/images/dishes/4.svg',
+  5: '/images/dishes/5.svg',
+  6: '/images/dishes/6.svg',
+  7: '/images/dishes/7.svg',
+  8: '/images/dishes/8.svg',
+  9: '/images/dishes/9.svg',
+  10: '/images/dishes/10.svg',
 }
 
 export const AVATAR_IMAGES = {
-  1: '/images/avatars/1.jpg',
-  2: '/images/avatars/2.jpg',
-  3: '/images/avatars/3.jpg',
+  1: '/images/avatars/1.svg',
+  2: '/images/avatars/2.svg',
+  3: '/images/avatars/3.svg',
 }
